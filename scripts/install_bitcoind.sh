feafbfdbcd3918710d6b076e472a7d848c2662e2
Matthew Zipkin <pinheadmz@pm.me>
2025-10-28 14:26:28 -0400

    test 2025_threadpool_http_server

--- scripts/install_bitcoind.sh
+++ scripts/install_bitcoind.sh
@@ -3,7 +3,7 @@
 set -ev
 
 sudo apt-get install -y build-essential cmake pkgconf python3 libevent-dev libboost-dev libsqlite3-dev libzmq3-dev
-git clone --depth 1 --branch http-rewrite-13march2025 https://github.com/pinheadmz/bitcoin
+git clone --depth 1 --branch 2025_threadpool_http_server https://github.com/furszy/bitcoin-core
 cd bitcoin
 cmake -B build -DWITH_BDB=OFF -DBUILD_GUI=OFF -DWITH_ZMQ=ON -DBUILD_BENCH=OFF -DBUILD_FUZZ_BINARY=OFF -DBUILD_GUI_TESTS=OFF -DBUILD_TESTS=OFF -DBUILD_UTIL=OFF -DBUILD_TX=OFF -DBUILD_WALLET_TOOL=OFF
 cmake --build build
